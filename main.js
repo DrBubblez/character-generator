6b46389e8f6e12d6f37cd8fd02a40b4868743cad
Harry <harrisonminer59@gmail.com>
2025-04-11 10:39:42 -0400

    started the main script and cretaed the data arrays for names

---
+++ main.js
@@ -0,0 +1,54 @@
+const maleFirstNames = [
+    "James", "John", "Robert", "Michael", "William",
+    "David", "Richard", "Joseph", "Thomas", "Charles",
+    "Christopher", "Daniel", "Matthew", "Anthony", "Mark",
+    "Donald", "Steven", "Paul", "Andrew", "Joshua",
+    "Kenneth", "Kevin", "Brian", "George", "Edward",
+    "Ronald", "Timothy", "Jason", "Jeffrey", "Ryan",
+    "Jacob", "Gary", "Nicholas", "Eric", "Jonathan",
+    "Stephen", "Larry", "Justin", "Scott", "Brandon",
+    "Benjamin", "Samuel", "Gregory", "Frank", "Alexander",
+    "Patrick", "Raymond", "Jack", "Dennis", "Jerry",
+    "Tyler", "Aaron", "Henry", "Adam", "Nathan",
+    "Douglas", "Zachary", "Peter", "Kyle", "Walter",
+    "Ethan", "Jeremy", "Harold", "Keith", "Christian",
+    "Roger", "Terry", "Sean", "Arthur", "Austin",
+    "Noah", "Carl", "Lawrence", "Dylan", "Jordan"
+]; // 75 Male First Names
+
+const femaleFirstNames = [
+    "Mary", "Patricia", "Jennifer", "Linda", "Elizabeth",
+    "Barbara", "Susan", "Jessica", "Sarah", "Karen",
+    "Nancy", "Lisa", "Margaret", "Betty", "Sandra",
+    "Ashley", "Dorothy", "Kimberly", "Emily", "Donna",
+    "Michelle", "Carol", "Amanda", "Melissa", "Deborah",
+    "Stephanie", "Rebecca", "Sharon", "Laura", "Cynthia",
+    "Kathleen", "Amy", "Shirley", "Angela", "Helen",
+    "Anna", "Brenda", "Pamela", "Nicole", "Emma",
+    "Samantha", "Katherine", "Christine", "Debra", "Rachel",
+    "Catherine", "Carolyn", "Janet", "Ruth", "Maria",
+    "Heather", "Diane", "Virginia", "Julie", "Joyce",
+    "Victoria", "Olivia", "Kelly", "Christina", "Lauren",
+    "Joan", "Evelyn", "Judith", "Megan", "Cheryl",
+    "Andrea", "Hannah", "Martha", "Jacqueline", "Frances",
+    "Gloria", "Ann", "Teresa", "Kathryn", "Sara"
+]; // 75 Female First Names
+
+const lastNames = [
+    "Stormwraithe", "Ironvalor", "Shadowmourn", "Brightward", "Dusksong",
+    "Flameborne", "Ravenhallow", "Silvershard", "Thornwylde", "Frostbane",
+    "Blackbriar", "Goldhaven", "Moonlance", "Stonecleft", "Windreaver",
+    "Ashenvyre", "Grimhowl", "Hollowmere", "Starwhisper", "Nightthorn",
+    "Drakenshield", "Oakenwrath", "Winterthorn", "Firevein", "Darkhollow",
+    "Sunforge", "Wolfshade", "Ironbark", "Silverhollow", "Stormquill",
+    "Dreadhallow", "Brightpyre", "Shadowglen", "Frostwraithe", "Ravenfyre",
+    "Thunderbane", "Goldbloom", "Moonspire", "Stonewylde", "Windshard",
+    "Ashenquill", "Grimspire", "Hollowthorn", "Starborne", "Nightpyre",
+    "Drakensong", "Oakensoul", "Winterwylde", "Firethorn", "Darkspire",
+    "Shadowforge", "Ironshade", "Frostwhisper", "Brightthorn", "Duskwind",
+    "Flamewhisper", "Ravenstone", "Silverthorn", "Thornspire", "Frostshadow",
+    "Blackthorn", "Goldspire", "Moonshadow", "Stoneforge", "Windthorn",
+    "Ashenbane", "Grimthorn", "Hollowshade", "Starspire", "Nightforge",
+    "Drakenbane", "Oakenshield", "Wintershade", "Firewhisper", "Darkthorn",
+    "Shadowbane", "Ironspire", "Frostglade", "Brightshade", "Duskforge"
+]; // 75 Last Names for Fantasy Characters
